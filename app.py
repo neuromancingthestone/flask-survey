7642e33709b5f2a1fc96491853e6b9ad7c64ccd6
robleo <releonardfl@gmail.com>
2024-02-04 11:52:23 -0500

    Added session code

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
-responses = []
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -14,15 +13,26 @@ def survey_page():
     """Survey base page"""
     return render_template("survey_page.html", survey=satisfaction_survey)
 
-@app.route("/questions", methods=["POST"])
-def questions_page():
-    """Start questions"""
+@app.route("/start", methods=["POST"])
+def start_vars():
+    """Initialize the responses list as a session cookie"""
+    session['responses'] = []
     return redirect("/questions/0")
+
+# @app.route("/questions", methods=["POST"])
+# def questions_page():
+#     """Start questions"""
+#    return redirect("/questions/0")
 
 @app.route("/questions/<int:id>")
 def question(id):
     """Show actual question based on ID.
     This uses <int:x> to specify it is an int"""
+    responses = session.get('responses')
+    print(responses)
+
+    if(responses is None):
+        return redirect("/")
 
     if(len(responses) != id):
         flash("YOU NEED TO ANSWER THIS QUESTION FIRST!")
@@ -38,7 +48,10 @@ def handle_question():
 
     choice = request.form['info']
 
+    """Appending list in session, need to rebind"""
+    responses = session['responses']
     responses.append(choice)
+    session['responses'] = responses
 
     if (len(responses) == len(satisfaction_survey.questions)):        
         return redirect("/done")
@@ -50,4 +63,4 @@ def handle_question():
 def done():
     """Thank person for finishing the survey and show what they said"""
 
-    return render_template("done.html", data=responses)    
+    return render_template("done.html", data=session['responses'])    
